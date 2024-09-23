4e863dbea278bb85366c9d7478baba598b099b4b
mobile-bungalow <pwmay.work@gmail.com>
2024-09-22 23:15:39 -0700

    compute tests

---
+++ src/wgpu_utils.rs
@@ -0,0 +1,48 @@
+use pollster::FutureExt as _;
+
+pub trait DeviceUtils {
+    fn wgpu_try<T, F>(&self, filter: wgpu::ErrorFilter, func: F) -> Result<T, wgpu::Error>
+    where
+        F: FnOnce(&wgpu::Device) -> T;
+
+    fn buffer_view<T, F>(&self, buffer: &wgpu::Buffer, func: F) -> T
+    where
+        F: FnOnce(Option<&[u8]>) -> T;
+}
+
+impl DeviceUtils for wgpu::Device {
+    fn wgpu_try<T, F>(&self, filter: wgpu::ErrorFilter, func: F) -> Result<T, wgpu::Error>
+    where
+        F: FnOnce(&wgpu::Device) -> T,
+    {
+        self.push_error_scope(filter);
+        let result = func(self);
+        match self.pop_error_scope().block_on() {
+            Some(error) => Err(error),
+            None => Ok(result),
+        }
+    }
+
+    /// funs the function on Some(&[u8]) if it the buffer can be mapped,
+    /// and none otherwise
+    fn buffer_view<T, F>(&self, buffer: &wgpu::Buffer, func: F) -> T
+    where
+        F: FnOnce(Option<&[u8]>) -> T,
+    {
+        if buffer.usage().contains(wgpu::BufferUsages::MAP_READ) {
+            let buffer_slice = buffer.slice(..);
+            buffer_slice.map_async(wgpu::MapMode::Read, |_| {});
+            self.poll(wgpu::Maintain::Wait);
+            let results = buffer_slice.get_mapped_range();
+
+            let res = func(Some(results.as_ref()));
+
+            std::mem::drop(results);
+            buffer.unmap();
+
+            res
+        } else {
+            func(None)
+        }
+    }
+}
---
+++ tests/compute_exeution.rs
@@ -0,0 +1,162 @@
+use kinnara::{ComputeReflectionContext, DeviceUtils};
+use wgpu::util::{BufferInitDescriptor, DeviceExt};
+use wgpu::{BindGroupEntry, BindingResource, BufferUsages};
+
+const BASIC_EXEC: &str = r"
+#version 450
+
+layout(set=0, binding=0) buffer DataBuffer {
+    float data[];
+} buf;
+
+layout(push_constant) uniform PushConstants {
+    float add;
+};
+
+layout(local_size_x=32, local_size_y=1, local_size_z=1) in;
+void main() {
+    uint index = gl_GlobalInvocationID.x;
+    data[index] += add;
+}
+";
+
+#[test]
+fn addition() -> Result<(), kinnara::Error> {
+    let (device, queue) = set_up_wgpu();
+    let source = compute_stage(BASIC_EXEC);
+
+    let mut refl = ComputeReflectionContext::new_compute(source)?;
+    let wg_size = refl.work_group_size("main").unwrap();
+    let compute_pipeline = refl.create_compute_pipeline(&device, "main", Default::default())?;
+
+    let length = 1048576;
+    let add = 5.0f32;
+    let data: Vec<_> = (0..length)
+        .map(|i| (i as f32).to_le_bytes())
+        .flatten()
+        .collect();
+
+    let ref buffer = device.create_buffer_init(&BufferInitDescriptor {
+        label: None,
+        // TODO: reflector should be able to provide minimum viable flags
+        usage: BufferUsages::UNIFORM
+            | BufferUsages::MAP_READ
+            | BufferUsages::COPY_DST
+            | BufferUsages::STORAGE,
+        // TODO: be able to provide the data for this through a serde json like object
+        contents: &data,
+    });
+
+    //TODO:  Make a helper function on the reflector for this
+    let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        label: None,
+        layout: &compute_pipeline.get_bind_group_layout(0),
+        entries: &[BindGroupEntry {
+            binding: 0,
+            resource: BindingResource::Buffer(wgpu::BufferBinding {
+                buffer,
+                offset: 0,
+                size: None,
+            }),
+        }],
+    });
+
+    let mut encoder = device.create_command_encoder(&Default::default());
+
+    {
+        let mut cpass = encoder.begin_compute_pass(&Default::default());
+        cpass.set_pipeline(&compute_pipeline);
+        // TODO make forgetting to do this unrepresentable state
+        cpass.set_push_constants(0, &add.to_le_bytes());
+        // TODO automate this if possible and make the dynamic offset less fallible
+        cpass.set_bind_group(0, &bind_group, &[]);
+        cpass.dispatch_workgroups(length / wg_size[0], wg_size[1], wg_size[2]);
+    }
+
+    queue.submit([encoder.finish()]);
+
+    let result_floats: Vec<_> = device.buffer_view(buffer, |slice| {
+        slice
+            .unwrap()
+            .chunks_exact(4)
+            .map(|chunk| f32::from_le_bytes([chunk[0], chunk[1], chunk[2], chunk[3]]))
+            .collect()
+    });
+
+    for (i, &value) in result_floats.iter().enumerate() {
+        assert_eq!(value, (i as f32 + add), "Mismatch at index {}", i);
+    }
+
+    Ok(())
+}
+
+fn compute_stage(src: &str) -> wgpu::ShaderSource {
+    wgpu::ShaderSource::Glsl {
+        shader: src.into(),
+        stage: wgpu::naga::ShaderStage::Compute,
+        defines: Default::default(),
+    }
+}
+
+fn set_up_wgpu() -> (wgpu::Device, wgpu::Queue) {
+    let instance = if cfg!(windows) {
+        let desc = wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::DX12,
+            ..Default::default()
+        };
+
+        wgpu::Instance::new(desc)
+    } else {
+        wgpu::Instance::default()
+    };
+
+    let adapter = pollster::block_on(async {
+        instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                force_fallback_adapter: false,
+                compatible_surface: None,
+            })
+            .await
+            .expect("Failed to find an appropriate adapter")
+    });
+    let mut required_limits = wgpu::Limits::default().using_resolution(adapter.limits());
+    required_limits.max_push_constant_size = 128;
+
+    let (d, q) = pollster::block_on(async {
+        adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    label: None,
+                    required_features: wgpu::Features::PUSH_CONSTANTS
+                        | wgpu::Features::TEXTURE_ADAPTER_SPECIFIC_FORMAT_FEATURES
+                        | wgpu::Features::MAPPABLE_PRIMARY_BUFFERS
+                        | wgpu::Features::CLEAR_TEXTURE,
+                    required_limits,
+                    memory_hints: wgpu::MemoryHints::Performance,
+                },
+                None,
+            )
+            .await
+            .expect("Failed to create device")
+    });
+
+    d.on_uncaptured_error(Box::new(|e| match e {
+        wgpu::Error::Internal {
+            source,
+            description,
+        } => {
+            panic!("internal error: {source}, {description}");
+        }
+        wgpu::Error::OutOfMemory { .. } => {
+            panic!("Out Of GPU Memory! bailing");
+        }
+        wgpu::Error::Validation {
+            description,
+            source,
+        } => {
+            panic!("{description} : {source}");
+        }
+    }));
+    (d, q)
+}
